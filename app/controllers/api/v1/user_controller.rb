99c218facf96e309c3b6a94137b927d4f9edb3aa
Alan Kehoe <alankehoe111@gmail.com>
2013-12-10 00:36:24 +0000

    This syntax is nicer

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,36 +1,33 @@
-module Api
-  module V1
-    class UserController < ApplicationController
-      def index
-        @users = User.all
-      end
+class Api::V1::UserController < ApplicationController
+  def index
+    @users = User.all
+  end
 
-      def show
-        @user = User.find_by(params[:id])
-      end
+  def show
+    @user = User.find_by(params[:id])
+  end
 
-      def update
-        @user = User.find_by(params[:id])
-        User.update
-      end
+  def update
+    @user = User.find_by(params[:id])
+    User.update
+  end
 
-      def create
-        @user = User.new(user_params)
-        @user.save
-      end
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
 
-      def destroy
-        @user = User.find_by(params[:id])
-        @user.destroy
-      end
+  def destroy
+    @user = User.find_by(params[:id])
+    @user.destroy
+  end
 
-      private
+  private
 
-      def user_params
-        params.require(:user).permit(:first_name, :last_name, :email,
-                                     :password, :phone_no, :group_id,
-                                     :username, :permission_level)
-      end
-    end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email,
+                                 :password, :phone_no, :group_id,
+                                 :username, :permission_level)
   end
 end
+
