fe7dc1463d9172c5e8c81d93db591057a13b64a8
Alan Kehoe <alankehoe111@gmail.com>
2013-12-10 16:18:18 +0000

    cleanup controller

--- app/controllers/v1/user_controller.rb
+++ app/controllers/v1/user_controller.rb
@@ -1,33 +1,35 @@
-class V1::UserController < ApplicationController
-  def index
-    @users = User.all
-  end
+module V1
+  class UserController < ApplicationController
+    def index
+      @users = User.all
+    end
 
-  def show
-    @user = User.find_by(params[:id])
-  end
+    def show
+      @user = User.find_by(params[:id])
+    end
 
-  def update
-    @user = User.find_by(params[:id])
-    User.update
-  end
+    def update
+      @user = User.find_by(params[:id])
+      User.update
+    end
 
-  def create
-    @user = User.new(user_params)
-    @user.save
-  end
+    def create
+      @user = User.new(user_params)
+      @user.save
+    end
 
-  def destroy
-    @user = User.find_by(params[:id])
-    @user.destroy
-  end
+    def destroy
+      @user = User.find_by(params[:id])
+      @user.destroy
+    end
 
-  private
+    private
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email,
-                                 :hashed_password, :phone_no, :group_id,
-                                 :username, :permission_level)
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email,
+                                   :hashed_password, :phone_no, :group_id,
+                                   :username, :permission_level)
+    end
   end
 end
 
