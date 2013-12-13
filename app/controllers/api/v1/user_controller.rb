b89b380af74cf22f1fb297d72d59df434b20a5d1
Alan Kehoe <alankehoe111@gmail.com>
2013-12-13 16:27:11 +0000

    small refactor

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -4,7 +4,7 @@ module Api
       api :GET, '/users'
       description 'Returns all the users in the database that the currently logged in user can see, based on Auth token.'
       param :auth_token, String, :desc => 'Users Auth token', :required => true
-      example File.read("#{Rails.root}/public/docs/api_docs/v1/user/index.json")
+      example File.read("#{Rails.root}/public/api_docs/v1/user/index.json")
       def index
         @users = User.all
       end
@@ -12,7 +12,7 @@ module Api
       api :GET, '/users/:id'
       description 'Returns the users with an id of :id in the database'
       param :auth_token, String, :desc => 'Users Auth token', :required => true
-      example File.read("#{Rails.root}/public/docs/api_docs/v1/user/show.json")
+      example File.read("#{Rails.root}/public/api_docs/v1/user/show.json")
       def show
         @user = User.find_by(params[:id])
       end
