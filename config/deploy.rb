b629ef1cd36800d010b4eaa8ccf4c73836fcfb6a
Alan Kehoe <alankehoe111@gmail.com>
2013-12-17 20:50:35 +0000

    Clear cache and only work with master branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :deploy_to, '/home/fleetdeploy/'
 set :tmp_dir, '/home/fleetdeploy/tmp'
 
 set :scm, :git
-set :branch, 'devise'
+set :branch, 'master'
 set :repo_url, 'git@git.mahala.co:fleetonrails/fleet-api.git'
 
 # set :format, :pretty
@@ -35,9 +35,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+         execute :rake, 'cache:clear'
+      end
     end
   end
 
