babcfd455a8e86f7e20a081a602f45489c300178
Alan Kehoe <alankehoe111@gmail.com>
2013-12-17 21:22:49 +0000

    Database

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,6 @@ set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
 set :keep_releases, 5
 
 namespace :deploy do
