a648ec967bb1ebedef3c19cd5f3eeb123c3f7203
Alan Kehoe <alankehoe111@gmail.com>
2013-12-25 00:52:16 +0000

    Nicer layout for docs

---
+++ spec/controllers/api_docs_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe ApiDocsController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      pending("something else getting finished")
+      this_should_not_get_executed
+    end
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      pending("something else getting finished")
+      this_should_not_get_executed
+    end
+  end
+
+end
