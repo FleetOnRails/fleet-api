c8ff411a178d8d5fcaad8120c6d53bbaaf7f26f7
Alan Kehoe <alankehoe111@gmail.com>
2013-12-28 19:23:33 +0000

    case sensitive

--- app/controllers/api_docs_controller.rb
+++ app/controllers/api_docs_controller.rb
@@ -1,7 +1,7 @@
 class ApiDocsController < ApplicationController
   def index
     @category = params[:category]
-    @category = 'README' if @category.blank?
+    @category = 'readme' if @category.blank?
 
     if File.exists?("#{Rails.root}/doc/api/#{@category}.md")
       render 'api_docs/index'
