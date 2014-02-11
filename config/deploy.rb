3b43f4162799ffe9ab07aca2068536208193f537
Alan Kehoe <alankehoe111@gmail.com>
2014-02-11 13:08:05 +0000

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,6 @@ set :application, 'fleet-api'
 set :stages, %w(production development)
 set :default_stage, 'development'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
 set :deploy_to, '/home/fleet/api/'
 set :tmp_dir, '/home/fleet/api/tmp'
 
@@ -11,14 +9,8 @@ set :scm, :git
 # set :branch, 'master'
 set :repo_url, 'git@git.raven.com:fleetonrails/fleet-api.git'
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
 set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
 namespace :deploy do
@@ -26,7 +18,6 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
       execute "mkdir #{current_path}/tmp"
       execute :touch, release_path.join('tmp/restart.txt')
     end
@@ -34,13 +25,9 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      #within release_path do
-      #   execute :rake, 'cache:clear'
-      #end
+
     end
   end
 
   after :finishing, 'deploy:cleanup'
-
 end
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,34 +1,11 @@
 set :stage, :development
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-#role :app, %w{fleet@app.raven.com}
-#role :web, %w{fleet@app.raven.com}
-#role :db, %w{fleet@app.raven.com}
-
 server 'fleet@app.raven.com',
        user: 'fleet',
        roles: %w{web app db},
        ssh_options: {
-           user: 'fleet', # overrides user setting above
+           user: 'fleet',
            forward_agent: true,
            auth_methods: %w(password),
            password: '?K?3wKP-asz^BrZ'
        }
-#server 'iefapi002.fleetonrails.eu',
-#       user: 'iefapi002',
-#       roles: %w{web app},
-#       ssh_options: {
-#           user: 'iefapi002', # overrides user setting above
-#           #     keys: %w(/home/user_name/.ssh/id_rsa),
-#           forward_agent: true,
-#           auth_methods: %w(publickey password),
-#           password: 'Fleetdeploy2013'
-#       }
-
-# setting per server overrides global ssh_options
-
-# fetch(:default_env).merge!(rails_env: :development)
