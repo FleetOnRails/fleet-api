ecfd51a78983dd361f2991e023a1baf2ab9f7cb2
Alan Kehoe <alankehoe111@gmail.com>
2014-02-17 13:39:51 +0000

    CORS

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -26,7 +26,11 @@ module V1
     end
 
     def set_cross_domain_header
-      response.headers['Access-Control-Allow-Origin'] # = '*' if Rails.development?
+      #response.headers['Access-Control-Allow-Origin'] # = '*' if Rails.development?
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
   end
 end
