57e8cc284d7ad48005e0092db6febf3aa4ed9335
Alan Kehoe <alankehoe111@gmail.com>
2014-02-26 00:31:35 +0000

    Make use of helper methods

--- app/controllers/v1/me_controller.rb
+++ app/controllers/v1/me_controller.rb
@@ -2,6 +2,8 @@ module V1
   class MeController < BaseController
     doorkeeper_for [:all]
 
+    include MeHelper
+
     def index
       @current_user
 
@@ -15,7 +17,7 @@ module V1
         @current_user.email = params[:email]
         @current_user.phone_no = params[:phone_no]
         @current_user.password = params[:password] || @current_user.password
-        @current_user.avatar = build_avatar
+        @current_user.avatar = MeHelper.build_avatar(params[:avatar_data], params[:avatar_extension])
         @current_user.save!
       else
         @current_user.first_name = params[:first_name]
@@ -27,15 +29,6 @@ module V1
       end
 
       respond_with @current_user
-    end
-
-    private
-
-    def build_avatar
-      tempfile = Tempfile.new ['upload', params[:avatar_extension]]
-      tempfile.binmode
-      tempfile.write(Base64.decode64(params[:avatar_data]))
-      tempfile
     end
   end
 end
--- app/helpers/v1/me_helper.rb
+++ app/helpers/v1/me_helper.rb
@@ -1,4 +1,12 @@
 module V1
   module MeHelper
+    class << self
+      def build_avatar(data, extension)
+        tempfile = Tempfile.new ['upload', extension]
+        tempfile.binmode
+        tempfile.write(Base64.decode64(data))
+        tempfile
+      end
+    end
   end
 end
