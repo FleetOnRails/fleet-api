c62cd04ad238ac32c2e4ab79cb858fd11a4dcc19
Alan Kehoe <alankehoe111@gmail.com>
2014-03-03 11:02:38 +0000

    Remove concerns

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,17 @@
 class Location < ActiveRecord::Base
-  include LocationConcern
-
   belongs_to :locationable, polymorphic: true
+
+  after_validation :geocode
+  after_validation :reverse_geocode
+
+  geocoded_by :address
+
+  reverse_geocoded_by :latitude, :longitude do |obj, results|
+    if geo = results.first
+      obj.address = geo.address
+      obj.city = geo.city
+      obj.county = geo.county
+      obj.country = geo.country_code
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,57 @@
 class User < ActiveRecord::Base
-  include UserConcern
-
   has_many :cars, as: :owner, dependent: :destroy
   has_many :vendors, as: :venderable, dependent: :destroy
 
   has_many :user_groups
   has_many :groups, through: :user_groups
+
+  mount_uploader :avatar, AvatarUploader
+
+  EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
+
+  validates_presence_of :email, :username, :first_name, :last_name
+  validates_presence_of :password, on: :create
+  validates_uniqueness_of :username, :email
+
+  validates_format_of :email, with: EMAIL_REGEX
+
+  before_create :encrypt_password
+  after_create :clear_password
+
+  class << self
+    def authenticate(username_or_email, login_password)
+      if EMAIL_REGEX.match(username_or_email)
+        user = User.find_by_email(username_or_email)
+      else
+        user = User.find_by_username(username_or_email)
+      end
+
+      if user && user.match_password(login_password)
+        user
+      else
+        false
+      end
+    end
+  end
+
+  def has_password?
+    password == encrypt_password
+  end
+
+  def match_password(login_password)
+    password == BCrypt::Engine.hash_secret(login_password, salt)
+  end
+
+  def encrypt_password
+    unless salt
+      if password.present?
+        self.salt = BCrypt::Engine.generate_salt
+        self.password= BCrypt::Engine.hash_secret(password, salt)
+      end
+    end
+  end
+
+  def clear_password
+    self.password = nil
+  end
 end
