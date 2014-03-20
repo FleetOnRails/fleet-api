2b13980b6d6a04688499d005d25716a9de7f9ca9
Alan Kehoe <alankehoe111@gmail.com>
2014-03-20 23:47:09 +0000

    remove old docs stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ FleetOnRails::Application.routes.draw do
   use_doorkeeper
 
   #
-  # Help
-  #
-  get '/' => 'help#index'
-  get '/api' => 'api_docs#index'
-  get '/api/:category' => 'api_docs#index', as: 'help_api_file'
-
-  #
   # API
   #
   namespace :v1 do
