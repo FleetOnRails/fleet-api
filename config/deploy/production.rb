55f50d31b018dd24667ea3814d9ffe5296d4aa40
Alan Kehoe <alankehoe111@gmail.com>
2014-03-20 16:51:02 +0000

    Production deploy for api

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,20 @@
 set :stage, :production
 
-server 'fleet@app.raven.com',
-       user: 'fleet',
+server 'fapi001@app.raven.com',
+       user: 'fapi001',
        roles: %w{web app db},
        ssh_options: {
-           user: 'fleet',
+           user: 'fapi001',
+           forward_agent: true,
+           keys: %w(~/.ssh/id_rsa),
+           auth_methods: %w(publickey)
+       }
+
+server 'fapi002@app.raven.com',
+       user: 'fapi002',
+       roles: %w{web app db},
+       ssh_options: {
+           user: 'fapi002',
            forward_agent: true,
            keys: %w(~/.ssh/id_rsa),
            auth_methods: %w(publickey)
