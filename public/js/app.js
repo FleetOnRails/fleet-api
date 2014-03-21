868ab5edcc0b4de4b978ee556e3bc705cfdb5355
Alan Kehoe <alankehoe111@gmail.com>
2014-03-21 09:46:49 +0000

    Update docs to be easier to navigate

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,20 @@ angular.module('fleetonrails-api', [
         'ngRoute',
         'ui.bootstrap',
         'fleetonrails-api.controllers',
+        'fleetonrails-api.directives',
         'hljs'
     ])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.
-            when('/', {templateUrl: 'partials/docs.html'}).
+            when('/docs', {templateUrl: 'partials/docs.html'}).
+            when('/docs/oauth', {templateUrl: 'partials/docs/api/oauth.html'}).
+            when('/docs/me', {templateUrl: 'partials/docs/api/me.html'}).
+            when('/docs/users', {templateUrl: 'partials/docs/api/users.html'}).
+            when('/docs/cars', {templateUrl: 'partials/docs/api/cars.html'}).
+            when('/docs/service_records', {templateUrl: 'partials/docs/api/service_records.html'}).
+            when('/docs/vendors', {templateUrl: 'partials/docs/api/vendors.html'}).
+            when('/docs/groups', {templateUrl: 'partials/docs/api/groups.html'}).
             otherwise({redirectTo: '/'});
     }])
 
--- public/js/directives.js
+++ public/js/directives.js
@@ -3,9 +3,11 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('fleetonrails-api.directives', [])
+
+    .directive('navbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/navbar.html'
+        }
+    });
