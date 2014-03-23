a49f0d10498727d66641136219de3153760dc9f9
Alan Kehoe <alankehoe111@gmail.com>
2014-03-23 14:33:36 +0000

    Limit the amount of records, this will give us some history maybe even a week or more which will be nice if the car gets stolen or something you can track back a week. will be better than the Malaysian airlines system :+1:

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,13 @@
 class Car < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
 
-  has_many :diagnostic_statistics, dependent: :destroy
   has_many :diagnostic_faults, dependent: :destroy
-  has_many :gps_statistics, dependent: :destroy
   has_many :service_records, dependent: :destroy
   has_many :documents, as: :documentable, dependent: :destroy
   has_many :fuel_entries, dependent: :destroy
+
+  has_many :diagnostic_statistics, dependent: :destroy, before_add: :limit_diagnostic_statistics
+  has_many :gps_statistics, dependent: :destroy, before_add: :limit_gps_statistics
 
   mount_uploader :avatar, AvatarUploader
 
@@ -15,4 +16,22 @@ class Car < ActiveRecord::Base
   validates_presence_of :registration
 
   validates_uniqueness_of :registration
+
+  private
+
+  LIMIT = 30000
+
+  def limit_gps_statistics
+    if gps_statistics.size >= LIMIT
+      first_thousand = self.gps_statistics.limit(1000)
+      first_thousand.each { |record| record.destroy!}
+    end
+  end
+
+  def limit_diagnostic_statistics
+    if things.size >= LIMIT
+      first_thousand = self.diagnostic_statistics.limit(1000)
+      first_thousand.each { |record| record.destroy!}
+    end
+  end
 end
