80cf7cc6bfb0cf9ad9261e458f369c6e5eaa907f
Alan Kehoe <alankehoe111@gmail.com>
2014-04-07 10:51:39 +0100

    Fix seed data to ease the db reset

--- db/fixtures/development/10_oauth_applications.rb
+++ db/fixtures/development/10_oauth_applications.rb
@@ -1,14 +1,14 @@
 web_application = Doorkeeper::Application.new(
     :name => 'Web Application',
-    :uid => '73bb593a38b4dc659c189ec6749fc1fd323330b12d86f1f25f4e2d3575c8d1ac',
-    :secret => 'cbbbf066762f56a4b97f7f85791156d6f1a0bb2b0901fa4dae6285179faa13bd',
+    :uid => '7236d09f6fd8210ea98914b603cbf4b3b10f6ef3ad8ad308e9f24b4a326cabb4',
+    :secret => '6080523131ac5b51fd73bd1bfe073658de7684b1d497cff6c789d0b8adf17a82',
     :redirect_uri => 'urn:ietf:wg:oauth:2.0:oob:fleetonrails-api'
 )
 
 android_application = Doorkeeper::Application.new(
     :name => 'Android Application',
-    :uid => 'fafd692a4a7415ef0095eb92591550aff5c14e751302a34e9d1ec6a8f7b621f9',
-    :secret => '8ad41adce09596458f6fedb5df101454c9a3470073ac49196135cc81cf89d21e',
+    :uid => 'bb44bd52efb7af50e9b12a14ab9b5059f9650ed0fad04f29e1dc0838a3655492',
+    :secret => '25ad5c1ecb760bb859fb96def8a72e506b9e73fb040b7208c88292e16cbb2a70',
     :redirect_uri => 'urn:ietf:wg:oauth:2.0:oob:fleetonrails-api'
 )
 
