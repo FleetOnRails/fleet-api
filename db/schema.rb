16b029c2e67477fc77ec98d6f562442972fac748
Alan Kehoe <alankehoe111@gmail.com>
2014-05-20 21:35:15 +0100

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520191603) do
+ActiveRecord::Schema.define(version: 20140520193952) do
 
   create_table "cars", force: true do |t|
     t.string   "make"
